3f2380d9289c3f659f32fafa12064f7ec31bedd7
mateusmoutinho <mateusmoutinho01@gmail.com>
2021-12-18 09:21:30 -0300

    refactoring name

--- main_test.py
+++ main_test.py
@@ -1,15 +1,17 @@
 # -------------_Extras-------------------------
-from tests.test_extras import *
+
+
+from uni_tests.test_extras import *
 # --------------ListArgs---------------------
-from tests.test_list_args import *
+from uni_tests.test_list_args import *
 # ---------------Args------------------------
-from tests.args.construction import *
-from tests.args.comparations import *
-from tests.args.test_args_flags_content import *
-from tests.args.others_args_methods import *
+from uni_tests.args.construction import *
+from uni_tests.args.comparations import *
+from uni_tests.args.test_args_flags_content import *
+from uni_tests.args.others_args_methods import *
 
 # ---------------FlagsContent----------------
-from tests.test_flags_content import *
+from uni_tests.test_flags_content import *
 
 from unittest import main
 
