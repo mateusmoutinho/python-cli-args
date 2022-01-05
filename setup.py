78a668dd40508a97805c7feb91b1b4cb9fb1bd6b
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-01-05 11:14:51 -0300

    uploading to pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'cli-args-system',        
   packages = ['cli_args_system'],
-  version = '0.2',    
+  version = '1.0',    
   license='MIT',     
   description = 'A Cli flags libary  to control argv flags and content',   
   author = 'Mateus Moutinho Queiroz',               
@@ -12,7 +12,7 @@ setup(
   keywords = ['ARGV', 'ClI ARGUMENTS'], 
 
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
